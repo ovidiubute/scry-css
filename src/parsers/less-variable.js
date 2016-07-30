81b6cf3424eaded6cbaa8c9e61ef9201bc17bf89
ovidiubute <ovidiu.bute@gmail.com>
2016-07-30 15:49:13 +0300

    Remove regexp.test call and fix destructuring.

--- src/parsers/less-variable.js
+++ src/parsers/less-variable.js
@@ -1,14 +1,11 @@
 const LESS_VARIABLE_REGEX = /^@([a-z0-9\-_]*):(\s*)([#\(\):,%a-z0-9]*);/i
 
 function parse(line) {
-  if (LESS_VARIABLE_REGEX.test(line)) {
-    const [all, variable, separator, value] = LESS_VARIABLE_REGEX.exec(line)
-    return {
-      variable,
-      value
-    }
-  }
-  return null
+  const [all, variable, separator, value] = LESS_VARIABLE_REGEX.exec(line) || []
+  return all ? {
+    variable,
+    value
+  } : null
 }
 
 module.exports = {
