b1498585ca76f89b4057f9a40bfc39c6a6dd16e4
ovidiubute <ovidiu.bute@gmail.com>
2016-07-31 09:32:34 +0300

    Add extension support.

--- src/io/dir-walker.js
+++ src/io/dir-walker.js
@@ -3,20 +3,22 @@ const Deferred = require('promised-io/promise').Deferred
 const path = require('path')
 const _ = require('lodash')
 
-function files(dirPath) {
+function files(dirPath, fileExtension) {
+  if (_.isEmpty(fileExtension)) {
+    throw new Error('fileExtension is mandatory!')
+  }
+
   let deferred = new Deferred()
 
-  dir.files(path.resolve(dirPath), function(err, files) {
+  dir.files(path.resolve(dirPath), (err, files) => {
     if (err) {
       deferred.reject(err)
       return
     }
 
-    files = files.filter(function (file) {
-       return !_.startsWith(file, '.') && _.endsWith(file, '.less')
-    })
-
-    deferred.resolve(files)
+    deferred.resolve(files.filter((file) => {
+      return !_.startsWith(file, '.') && _.endsWith(file, fileExtension)
+    }))
   })
 
   return deferred.promise
--- test/io/dir-walker.js
+++ test/io/dir-walker.js
@@ -4,23 +4,30 @@ const fixture = require('../fixtures/walk-directory/base')
 describe('DirWalker', () => {
   describe('#parse', () => {
     it('should return all LESS file names from a directory', (done) => {
-      DirWalker.files('test/fixtures/walk-directory').then((files) => {
+      DirWalker.files('test/fixtures/walk-directory', '.less').then((files) => {
+        expect(files.length).to.equal(fixture.files.length)
         for (let i = 0; i < files.length; i++) {
           expect(files[i]).to.have.string(fixture.files[i])
         }
 
         done()
+      }, (err) => {
+        assert.fail(false, true, err)
       })
     })
 
     it('should return error given a non-existent directory', (done) => {
-      DirWalker.files('test/fixtures/no-way-this-exists').then((files) => {
+      DirWalker.files('test/fixtures/no-way-this-exists', '.less').then((files) => {
         assert.fail(false, true, 'Should have rejected the promise.')
       }, (err) => {
         expect(err).to.be.instanceof(Error)
 
         done()
       })
+    })
+
+    it('should throw error if not given an extension', () => {
+      expect(DirWalker.files.bind(DirWalker, 'test/fixtures/walk-directory')).to.throw(Error)
     })
   })
 })
