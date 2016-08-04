e50ce930637894995cfb46558e20d34dd1f73f0f
ovidiubute <ovidiu.bute@gmail.com>
2016-08-04 22:43:35 +0300

    Add fuzzy search package.

---
+++ test/pipeline/find-suggestions.js
@@ -0,0 +1,7 @@
+const findSuggestions = require('../../src/pipeline/find-suggestions')
+
+describe('#findSuggestions', () => {
+  it('should suggest LESS variables', (done) => {
+    done()
+  })
+})
