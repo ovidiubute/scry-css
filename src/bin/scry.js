2f04019a303cc2945dd67ee72ef74cf17c440d1d
ovidiubute <ovidiu.bute@gmail.com>
2016-08-13 18:13:02 +0300

    Add bin script.

---
+++ src/bin/scry.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+const program = require('commander')
+const PipelineRunner = require('../pipeline/runner')
+
+program
+  .version('scry 0.1.0')
+  .usage('[options] <type> <dir> <file...>')
+  .option('-r --reporter [reporter]', 'Reporter', /^(console|json)$/i)
+  .parse(process.argv)
+
+if (!program.args.length) {
+  program.help()
+} else {
+  PipelineRunner.run(program.reporter || 'console', ...program.args).then((summary) => {
+    console.info(summary)
+  })
+}
