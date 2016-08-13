0e285b8f39b436c5b9624fb3505c34e80e364e84
ovidiubute <ovidiu.bute@gmail.com>
2016-08-13 18:13:13 +0300

    Add end-to-end runner.

---
+++ src/pipeline/runner.js
@@ -0,0 +1,34 @@
+const _ = require('lodash')
+const Promise = require('promised-io/promise')
+
+const { intelInput, intelExtract } = require('./intelli')
+const findCandidates = require('./find-candidates')
+const findSuggestions = require('./find-suggestions')
+const extractLines = require('./extract-lines')
+const summary = require('./summary')
+const techConfig = require('../config')
+
+function run(reporterType, techType, dirPath, ...filePaths) {
+  const runnerConfig = techConfig[techType]
+  if (_.isEmpty(runnerConfig)) {
+    throw new Error(`Unsupported techType: ${techType}`)
+  }
+
+  return Promise.allKeys({
+    intelliByFile: Promise.seq([
+      intelInput.bind(null, [dirPath], runnerConfig),
+      (filesByDirectory) => intelExtract(filesByDirectory, runnerConfig),
+    ]),
+    candidatesByFile: Promise.seq([
+      extractLines.bind(null, filePaths),
+      ({files: linesByFile}) => findCandidates(linesByFile),
+    ])
+  }).then((data) => {
+    const results = findSuggestions(_.extend(data, { config: runnerConfig }))
+    return summary(reporterType, results)
+  })
+}
+
+module.exports = {
+  run
+}
