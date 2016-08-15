1ee7b48282852d03ced72d967bcf0739a7cf2ffa
ovidiubute <ovidiu.bute@gmail.com>
2016-08-15 16:43:01 +0300

    Move code to its own function.

--- src/reporters/console-reporter.js
+++ src/reporters/console-reporter.js
@@ -4,18 +4,22 @@ function noSuggestions() {
   return 'Sorry, no suggestions available.'
 }
 
-module.exports = (results) => {
-  if (_.isEmpty(results.suggestionData)) {
-    throw new Error('Cannot format data!')
-  }
-
-  const resultCount = _.reduce(results.suggestionData, (totalCount, { propertyDefinitions }) => (
+function countSuggestions(results) {
+  return _.reduce(results.suggestionData, (totalCount, { propertyDefinitions }) => (
     totalCount + _.reduce(propertyDefinitions, (localCount, { suggestions }) => (
       localCount + suggestions.filter(({ suggestions: localSuggestions }) => (
         !_.isEmpty(localSuggestions)
       )).length
     ), 0)
   ), 0)
+}
+
+module.exports = (results) => {
+  if (_.isEmpty(results) || _.isEmpty(results.suggestionData)) {
+    throw new Error('Cannot format data!')
+  }
+
+  const resultCount = countSuggestions(results)
 
   if (resultCount === 0) {
     return noSuggestions()
