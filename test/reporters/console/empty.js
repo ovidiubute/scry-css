61d86a46140b871faecdb6b07fa600f8a8546fc0
Kumar Gaurav Pandey <gaurav1911@outlook.com>
2016-10-14 02:16:38 +0530

    Update empty.js

--- test/reporters/console/empty.js
+++ test/reporters/console/empty.js
@@ -1,7 +1,7 @@
 const consoleReporter = require('../../../src/reporters/console-reporter')
 const fixture = require('../../fixtures/reporters/console/empty')
 
-describe('fixtures/reporters/console/empty', () => {
+describe('/fixtures/reporters/console/empty', () => {
   it('should return description for 0 results', () => {
     expect(consoleReporter(fixture.input)).to.deep.equal(fixture.output)
   })
