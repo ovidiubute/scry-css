d6b2f664cce776fdb508c2d94d5182245a7d1d98
Kumar Gaurav Pandey <gaurav1911@outlook.com>
2016-10-14 01:42:02 +0530

    Update base.js

--- test/reporters/console/base.js
+++ test/reporters/console/base.js
@@ -1,7 +1,7 @@
 const consoleReporter = require('../../../src/reporters/console-reporter')
 const fixture = require('../../fixtures/reporters/console/base')
 
-describe('Console Reporter', () => {
+describe('reporters/console-reporter', () => {
   it('should return formatted description of results', () => {
     const actual = consoleReporter(fixture.input)
     expect(actual).to.deep.equal(fixture.output)
