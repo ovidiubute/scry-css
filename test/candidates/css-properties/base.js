cc58dbce5da59287d0f858a0ee39f12f71fdab90
Kumar Gaurav Pandey <gaurav1911@outlook.com>
2016-10-14 02:13:40 +0530

    Update base.js

--- test/candidates/css-properties/base.js
+++ test/candidates/css-properties/base.js
@@ -2,8 +2,8 @@ const CssPropertyCandidates = require('../../../src/candidates/css-properties')
 const inputFixture = require('../../fixtures/candidates/css-properties/base/input')
 const outputFixture = require('../../fixtures/candidates/css-properties/base/output')
 
-describe('candidates/css-properties', () => {
-  describe('fixtures/candidates/css-properties/base', () => {
+describe('/candidates/css-properties', () => {
+  describe('/fixtures/candidates/css-properties/base', () => {
     describe('#filter', () => {
       it('should return properties with anything other than LESS variables as values', () => {
         expect(CssPropertyCandidates.filter(inputFixture.data)).to.deep.equal(outputFixture.data)
