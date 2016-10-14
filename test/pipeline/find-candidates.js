7844cff69f5ace67c8299e6b079bd41eafc3e67a
Kumar Gaurav Pandey <gaurav1911@outlook.com>
2016-10-14 16:17:50 +0530

    Modify test describes to match file paths  (#34)
    * Update scry-css.js
    
    * Update find-candidates.js
    
    * Update extract-lines.js
    
    * Update find-suggestions.js
    
    * Update extract.js
    
    * Update input.js
    
    * Update variables.js
    
    * Update json-reporter.js
    
    * Update base.js
    
    * Update empty.js
    
    * Update base.js
    
    * Update no-props.js
    
    * Update no-props.js
    
    * Update css-property.js
    
    * Update sass-variable.js
    
    * Update dir-walker.js
    
    * Update line-reader.js
    
    * Update base.js
    
    * Update base.js
    
    * Update empty.js
    
    * Update no-props.js
    
    * Update find-suggestions.js
    
    * Update extract.js
    
    * Update input.js
    
    * Update css-property.js
    
    * Update sass-variable.js
    
    * Update dir-walker.js
    
    * Update line-reader.js
    
    * Update blacklist.js
    
    * Update base.js
    
    * Update variables.js
    
    * Update json-reporter.js
    
    * Update base.js
    
    * Update empty.js
    
    * Update no-props.js
    
    * Update scry-css.js
    
    * Update base.js
    
    * Update empty.js
    
    * Update no-props.js

--- test/pipeline/find-candidates.js
+++ test/pipeline/find-candidates.js
@@ -1,7 +1,7 @@
 const findCandidates = require('../../src/pipeline/find-candidates')
 const fixture = require('../fixtures/pipeline/find-candidates/base')
 
-describe('#findCandidates', () => {
+describe('/pipeline/find-candidates', () => {
   it('should return CSS properties that are candidates', () => {
     expect(findCandidates(fixture.input)).to.deep.equal(fixture.output)
   })
