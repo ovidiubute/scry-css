c6af7a1dc663800b89b18943486f21e13d118bf3
Kumar Gaurav Pandey <gaurav1911@outlook.com>
2016-10-14 15:34:08 +0530

    Update no-props.js

--- test/reporters/console/no-props.js
+++ test/reporters/console/no-props.js
@@ -1,7 +1,7 @@
 const consoleReporter = require('../../../src/reporters/console-reporter')
 const fixture = require('../../fixtures/reporters/console/empty')
 
-describe('/fixtures/reporters/console/empty', () => {
+describe('/reporters/console', () => {
   it('should return description for no results', () => {
     expect(consoleReporter(fixture.input)).to.deep.equal(fixture.output)
   })
